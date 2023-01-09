d58b778fed51017754fe6d4bb8f341e494768a1f
Robert O'Brien <robrien20@gmail.com>
2023-01-09 17:59:24 -0500

    add login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+     before_action :authenticate_user!
+
+        def after_sign_in_path_for(resource)
+            if current_user.admin?
+                admin_dashboard_path
+            else
+                root_path
+            end
+        end
+
+        def after_sign_out_path_for(resource)
+            root_path
+        end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
  get 'render/index'
 
  # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
